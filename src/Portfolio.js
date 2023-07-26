26265854f8df8e1c011f5dbacbfa003f24cffc43
Atsuki02 <canvanvam@gmail.com>
2023-07-26 14:15:03 -0700

    add portfolio

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -116,10 +116,25 @@ export const portfolioItems = [
   },
   {
     index: 3,
+    title: "Meating you",
+    description:
+      "This web application enables users to conveniently make reservations at a restaurant right from the comfort of their own home.",
+    img: "bg-[url('/images/portfolio3.png')]",
+    technologies: ["React Router", "Redux", "React Query", "Supabase"],
+    link: "https://meating-you.netlify.app/",
+    bgColor1: "bg-teal-200",
+    borderColor1: "lg:border-teal-500",
+    textColor1: "text-teal-500",
+    buttonBgColor: "bg-teal-400",
+    borderColor2: "border-teal-800",
+    bgColor3: "bg-teal-600",
+  },
+  {
+    index: 4,
     title: "usePopcorn",
     description:
       "This web application allows you to rate the movies you have watched and add them to your watched list.",
-    img: "bg-[url('/images/portfolio3.png')]",
+    img: "bg-[url('/images/portfolio4.png')]",
     technologies: ["React", "React Hook"],
     link: "",
     bgColor1: "bg-violet-200",
@@ -131,11 +146,11 @@ export const portfolioItems = [
   },
 
   {
-    index: 4,
+    index: 5,
     title: "The React app",
     description:
       "This is a quiz game application that challenges you to answer a series of questions about React within a limited time. Can you prove your knowledge by answering more than a dozen questions correctly?",
-    img: "bg-[url('/images/portfolio4.png')]",
+    img: "bg-[url('/images/portfolio5.png')]",
     technologies: ["React"],
     link: "",
     bgColor1: "bg-blue-200",
@@ -146,11 +161,11 @@ export const portfolioItems = [
     bgColor3: "bg-blue-600",
   },
   {
-    index: 5,
+    index: 6,
     title: "WorldWise",
     description:
       "This web application allows you to visually track and mark the places you have visited on a world map. It provides a visual representation of your travel journey across the globe.",
-    img: "bg-[url('/images/portfolio5.png')]",
+    img: "bg-[url('/images/portfolio6.png')]",
     technologies: ["React", "React Router"],
     link: "",
     bgColor1: "bg-emerald-200",
@@ -162,11 +177,11 @@ export const portfolioItems = [
   },
 
   {
-    index: 6,
+    index: 7,
     title: "Split-n-Bill",
     description:
       "This web application assists you in calculating and splitting bills when you need to share expenses with others. It helps you determine the total bill amount and how much each person owes, making it easier to manage shared expenses.",
-    img: "bg-[url('/images/portfolio6.png')]",
+    img: "bg-[url('/images/portfolio7.png')]",
     technologies: ["React"],
     link: "",
     bgColor1: "bg-orange-200",
@@ -177,11 +192,11 @@ export const portfolioItems = [
     bgColor3: "bg-orange-600",
   },
   {
-    index: 7,
+    index: 8,
     title: "Expense Track App",
     description:
       "This web application enables you to maintain a record of your monthly expenses, helping you keep track of your spending habits and how much you have spent. It provides a convenient way to monitor and manage your expenses effectively.",
-    img: "bg-[url('/images/portfolio7.png')]",
+    img: "bg-[url('/images/portfolio8.png')]",
     technologies: ["React"],
     link: "",
     bgColor1: "bg-purple-200",
@@ -192,11 +207,11 @@ export const portfolioItems = [
     bgColor3: "bg-purple-600",
   },
   {
-    index: 8,
+    index: 9,
     title: "Yakiniku Hokuden",
     description:
       "This website belongs to a BBQ restaurant, where we proudly present our services through enticing pictures that will tempt your taste buds and captivate your senses. ",
-    img: "bg-[url('/images/portfolio8.png')]",
+    img: "bg-[url('/images/portfolio9.png')]",
     technologies: ["HTML", "SASS", "Javascript"],
     link: "https://hokuden.verse.jp/",
     bgColor1: "bg-red-200",
